3960e8531b251286961dc23b80dab922b9ba7710
Halle Broadnax <broadnaxhalle@gmail.com>
2025-10-23 22:48:53 -0700

    Update logic create an AST from parsed grammar calculate result by recursively traversing the AST

--- calculator_cfg.py
+++ calculator_cfg.py
@@ -9,7 +9,58 @@ This script builds an AST using Lark and then evaluates it recursively.
 import sys
 import math
 from lark import Lark, Transformer
-from grammar import GRAMMAR  # import the grammar string
 
-# Build parser from grammar string
-parser = Lark(GRAMMAR, start='start', parser='lalr')
+class ASTCreator(Transformer):
+    def plus(self, items):
+        return('plus', items[0], items[1])
+    def sub(self, items):
+        return('sub', items[0], items[1])
+    def mul(self, items):
+        return('mul', items[0], items[1])
+    def neg(self, items):
+        return('neg', items[0])
+    def log(self, items):
+        return('log', items[0], items[1])
+    def exp(self, items):
+        return('exp', items[0], items[1])
+    def num(self, items):
+        return('num', int(items[0]))
+    
+def evaluate(ast):
+    if ast[0] == 'plus':
+        return evaluate(ast[1]) + evaluate(ast[2])
+    elif ast[0] == 'sub':
+        return evaluate(ast[1]) - evaluate(ast[2])
+    elif ast[0] == 'mul':
+        return evaluate(ast[1]) * evaluate(ast[2])
+    elif ast[0] == 'neg':
+        return evaluate(ast[1]) * -1
+    elif ast[0] == 'log':
+        return math.log(evaluate(ast[1]), evaluate(ast[2]))
+    elif ast[0] == 'exp':
+        return evaluate(ast[1]) ** evaluate(ast[2])
+    elif ast[0] == 'num':
+        return ast[1]
+    else:
+        raise ValueError(f"Unknown operation: {ast}")
+    
+def createParser():
+    with open("grammar.lark", "r") as grammar_file:
+        grammar = grammar_file.read()
+
+    return Lark(grammar, parser='lalr')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python calculator_cfg.py <expression>")
+        sys.exit(1)
+
+    ast_creator = ASTCreator()
+    parser = createParser()
+    expression = sys.argv[1]
+    tree = parser.parse(expression)
+    ast = ast_creator.transform(tree)
+    result = evaluate(ast)
+    print(result)
+
+    
---
+++ specs.md
@@ -0,0 +1,20 @@
+# Cpsc 354 Programming Assignment 1 — Calculator
+
+## Collaborators 
+- Savana Chou, sachou@chapman.edu
+- Kaye Galang, agalang@chapman.edu
+- Halle Broadnax, broadnax@chapman.edu
+
+## Files
+calculator_cfg.py
+grammar.lark
+specs.md
+
+## Running Instructions
+python calculator_cfg.py <"expression">
+
+## Specifications
+
+### calculator_cfg.py
+
+### grammar.lark
